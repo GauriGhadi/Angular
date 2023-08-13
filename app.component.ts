b8e27898cecaec0f994e9a902172de9b3a7502a4
GauriGhadi <95408409+GauriGhadi@users.noreply.github.com>
2023-08-13 19:59:38 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { User, UsersService } from './users.service';
+
+@Component({
+  selector: 'screen1',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  users: User[] = [];
+  title: any;
+  constructor(private userService: UsersService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((data) => {
+      this.users = data;
+      console.log('DATA' , this.users);
+    });
+    
+  }
+}
